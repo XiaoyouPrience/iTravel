4d6cf189c90eaa5023a9ccf71c030781926f167e
xiaoyou <xiaoyouPrince@163.com>
2016-03-11 02:48:39 +0800

    Initial Commit

---
+++ iTravel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTravel
+//
+//  Created by XiaoYou on 16/3/11.
+//  Copyright © 2016年 XY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iTravel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iTravel
+//
+//  Created by XiaoYou on 16/3/11.
+//  Copyright © 2016年 XY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
