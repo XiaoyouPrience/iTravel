1643f41b650975c3172b5a69328fb0bd9c6a7d94
xiaoyou <xiaoyouPrince@163.com>
2016-03-16 03:48:15 +0800

    添加中间加号按钮。

--- iTravel/Classes/Main/View/XYTabbar.m
+++ iTravel/Classes/Main/View/XYTabbar.m
@@ -11,11 +11,26 @@
 
 @interface XYTabbar ()
 
+@property (nonatomic,strong) NSMutableArray *tabBarButtons;
+
 @property (nonatomic,weak) XYTabbarButton *selectButton;
+
+@property (nonatomic,weak) UIButton *plusButton;
 
 @end
 
 @implementation XYTabbar
+
+/**
+ *  这个数组只需要一次，直接懒加载
+ */
+- (NSMutableArray *)tabBarButtons
+{
+    if (!_tabBarButtons) {
+        _tabBarButtons = [[NSMutableArray alloc] init];
+    }
+    return _tabBarButtons;
+}
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
@@ -26,6 +41,16 @@
         if (!iOS7) {
             self.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageWithName:@"tabbar_backGround"]];
         }
+        
+        // 添加一个中间的加号按钮
+        UIButton *plusButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [plusButton setBackgroundImage:[UIImage imageWithName:@"tabbar_compose_button" ]forState:UIControlStateNormal];
+        [plusButton setBackgroundImage:[UIImage imageWithName:@"tabbar_compose_button_highlighted"] forState:UIControlStateHighlighted];
+        [plusButton setImage:[UIImage imageWithName:@"tabbar_compose_icon_add"] forState:UIControlStateNormal];
+        [plusButton setImage:[UIImage imageWithName:@"tabbar_compose_icon_add_highlighted"] forState:UIControlStateHighlighted];
+        [self addSubview:plusButton];
+        plusButton.frame = CGRectMake(0, 0, plusButton.currentBackgroundImage.size.width, plusButton.currentBackgroundImage.size.height);
+        self.plusButton = plusButton;
         
     }
     return self;
@@ -38,18 +63,20 @@
  */
 - (void)addTabBarButtonWithItem:(UITabBarItem *)item
 {
-    // 创建按钮
+    // 1.创建按钮
     XYTabbarButton *button = [[XYTabbarButton alloc] init];
     [self addSubview:button];
     
-    // 设置数据
+    [self.tabBarButtons addObject:button];
+    
+    // 2.设置数据
     button.item = item;
     
-    // 监听按钮点击
+    // 3.监听按钮点击
     [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchDown];
     
-    // 设置默认点击第一个,判断添加到第一个按钮时，长度为1
-    if (self.subviews.count == 1) {
+    // 4.设置默认点击第一个,判断添加到第一个按钮时，长度为1
+    if (self.tabBarButtons.count == 1) {
         [self buttonClick:button];
     }
 }
@@ -77,16 +104,25 @@
 - (void)layoutSubviews{
     [super layoutSubviews];
     
+    // 1.加号按钮
+    CGFloat h = self.frame.size.height;
+    CGFloat w = self.frame.size.width;
+    self.plusButton.center = CGPointMake(w *0.5, h *0.5);
+    
+    // 2.重新布局所有按钮
     CGFloat buttonY = 0;
     CGFloat buttonW = self.frame.size.width / self.subviews.count;
     CGFloat buttonH = self.frame.size.height;
     
-    for (int index = 0; index < self.subviews.count; index ++) {
+    for (int index = 0; index < self.tabBarButtons.count; index ++) {
         // 取出按钮
-        XYTabbarButton *button = self.subviews[index];
+        XYTabbarButton *button = self.tabBarButtons[index];
     
         // 设置按钮的frame
         CGFloat buttonX = index * buttonW;
+        if (index > 1) {
+            buttonX = index * buttonW + buttonW;
+        }
         [button setFrame:CGRectMake(buttonX, buttonY, buttonW, buttonH)];
         
         // 绑定tag
