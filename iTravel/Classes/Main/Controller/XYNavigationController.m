e838bf90179e298f48d2d2dafdcb6ee30d08a8d7
xiaoyou <xiaoyouPrince@163.com>
2016-03-19 01:50:24 +0800

    设置了导航栏主题

--- iTravel/Classes/Main/Controller/XYNavigationController.m
+++ iTravel/Classes/Main/Controller/XYNavigationController.m
@@ -10,4 +10,65 @@
 
 @implementation XYNavigationController
 
+// 一个类只有在第一次调用的时候会调用一次。
++ (void)initialize
+{
+    // 1.设置导航栏主题
+    [self setupNavBarTheme];
+    
+    // 2.设置导航栏按钮主题
+    [self setupBarButtonItemTheme];
+    
+}
+
+/**
+ *  设置导航栏按钮主题
+ */
++ (void)setupBarButtonItemTheme
+{
+    UIBarButtonItem *item = [UIBarButtonItem appearance];
+    
+    [item setBackButtonBackgroundImage:[UIImage imageWithName:@"navigationbar_button_background"] forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    [item setBackButtonBackgroundImage:[UIImage imageWithName:@"navigationbar_button_background_pushed"] forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    [item setBackButtonBackgroundImage:[UIImage imageWithName:@"navigationbar_button_background_disable"] forState:UIControlStateDisabled barMetrics:UIBarMetricsDefault];
+}
+/**
+ *  设置导航栏主题
+ */
++ (void)setupNavBarTheme
+{
+    // 取出apperence对象
+    UINavigationBar *navBar = [UINavigationBar appearance];
+    
+    if (!iOS7) {
+        // 当ios7以上的时候，导航栏直接是64，两个是在一起的
+        [navBar setBackgroundImage:[UIImage imageWithName:@"navigationbar_background"] forBarMetrics:UIBarMetricsDefault];
+        // 避免状态栏被同化，重新设置状态栏颜色
+        [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleBlackOpaque;
+    }
+    
+    // 2.设置文字属性
+    NSMutableDictionary *textAttrs = [NSMutableDictionary dictionary];
+    textAttrs[UITextAttributeFont] = [UIFont systemFontOfSize:19];
+    textAttrs[UITextAttributeTextColor] = [UIColor blackColor];
+    textAttrs[UITextAttributeTextShadowColor] = [UIColor redColor];
+    textAttrs[UITextAttributeTextShadowOffset] = [NSValue valueWithUIOffset:UIOffsetZero ];
+    navBar.titleTextAttributes = textAttrs;
+}
+
+
+
+// 拦截子控制器的push方法，
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    // 当push栈中有控制器的时候就再隐藏bottomBar
+    if (self.viewControllers.count > 1) {
+        // 被push的ViewController隐藏BottomBar
+        viewController.hidesBottomBarWhenPushed = YES;
+    }
+    
+    // 重写父类的方法
+    [super pushViewController:viewController animated:animated];
+}
+
 @end
--- iTravel/Classes/WeiBo(微博)/Controller/XYWeiBoViewController.m
+++ iTravel/Classes/WeiBo(微博)/Controller/XYWeiBoViewController.m
@@ -8,7 +8,61 @@
 
 #import "XYWeiBoViewController.h"
 
+@interface XYWeiBoViewController()
+
+@end
+
 @implementation XYWeiBoViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"dianwo" style:UIBarButtonItemStyleBordered target:self action:@selector(itemClicked)];
+//    self.navigationItem.rightBarButtonItem.enabled = NO;
+    
+}
+- (void)itemClicked
+{
+    DLog(@"点击了我");
+}
+
+#pragma  --- mark TableViewDelegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // 默认为1
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // 1.创建cell
+    static NSString *cellID = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellID];
+    }
+    
+    // 2.给cell设置数据
+    cell.textLabel.text = @"hahah";
+    
+    // 3.返回cell
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view.backgroundColor = [UIColor redColor];
+    vc.hidesBottomBarWhenPushed = YES; // 这个不能每次都写，要拦截
+    [self.navigationController pushViewController:vc animated:YES];
+}
 
 
 @end
